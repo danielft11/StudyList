e890139a03a9269a6c7f395d71169084b6ab6e51
Vinicios Neves <vinicios.neves@mac>
2025-04-21 19:10:34 +0100

    video 1.4

--- src/components/Dialog/index.jsx
+++ src/components/Dialog/index.jsx
@@ -6,24 +6,23 @@ import './dialog.style.css'
 
 export const Dialog = () => {
     const dialog = document.querySelector("dialog");
-    const showButton = document.querySelector("dialog + button");
-    const closeButton = document.querySelector("dialog button");
-    
+
     // "Show the dialog" button opens the dialog modally
-    showButton.addEventListener("click", () => {
-      dialog.showModal();
-    });
-    
+    const openModal = () => {
+        console.log('vamos abrir a modal')
+        dialog.showModal();
+    };
+
     // "Close" button closes the dialog
-    closeButton.addEventListener("click", () => {
-      dialog.close();
-    });
+    const closeModal = () => {
+        dialog.close();
+    };
     return (<>
         <dialog>
-            <button autofocus>Close</button>
+            <button autoFocus onClick={closeModal}>Close</button>
             <p>This modal dialog has a groovy backdrop!</p>
         </dialog>
-        <button>Show the dialog</button>
+        <button onClick={openModal}>Show the dialog</button>
     </>)
 }
 
